ebce7dde3a04e668592d7777cf7e2dda94a1f584
Jorge Espada <espada.jorge@gmail.com>
2011-09-20 11:22:07 -0300

    fixed match in lxc guest detect

--- virtualization.rb
+++ virtualization.rb
@@ -102,7 +102,7 @@ if File.exists?("/proc/1/cpuset")
   else
     if File.exist?("/proc/1/environ")
     proc_environ = File.read("/proc/1/environ")
-      if proc_environ.match('/.*(lxc-start).*/')
+      if proc_environ.match('lxc-start')
         virtualization[:system] = "linux-lxc"
         virtualization[:role] = "guest"
       end
